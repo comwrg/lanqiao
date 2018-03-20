e0041e23a9bff02acc3cf8736b762bd446baf575
comwrg <xcomwrg@gmail.com>
2018-03-20 15:13:04 +0800

    add day2_3
    the logical part of the code i wrote with my feeling, then AC.
    so now maybe only the god knows.

---
+++ training/day2/3.cpp
@@ -0,0 +1,65 @@
+/**
+ * http://acm.hdu.edu.cn/showproblem.php?pid=1022
+ */
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.good()) { cin.rdbuf(fin.rdbuf()); freopen(filename.c_str(), "r", stdin); } }
+void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
+
+#define IN  0
+#define OUT 1
+int n;
+char data[2][10];
+vector<int> record(20);
+stack<char> stk;
+
+int main() {
+    rdIn("data.txt");
+
+    while (~scanf("%d", &n)) {
+        scanf("%s", data[IN]);
+        scanf("%s", data[OUT]);
+
+        record.clear();
+        while (!stk.empty()) {
+            stk.pop();
+        }
+        int i = 0, j = 0;
+        while (j < n) {
+            if (stk.empty()) {
+                if (i >= n) {
+                    break;
+                }
+                stk.push(data[IN][i++]);
+                record.push_back(IN);
+            }
+            if (stk.top() == data[OUT][j]) {
+                j++;
+                stk.pop();
+                record.push_back(OUT);
+            } else if (i < n) {
+                stk.push(data[IN][i++]);
+                record.push_back(IN);
+            } else {
+                break;
+            }
+        }
+
+        if (record.size() == 2 * n) {
+            printf("Yes.\n");
+            for (int v : record) {
+                if (v == IN) {
+                    printf("in\n");
+                } else {
+                    printf("out\n");
+                }
+            }
+        } else {
+            printf("No.\n");
+        }
+
+        printf("FINISH\n");
+    }
+
+    return 0;
+}
