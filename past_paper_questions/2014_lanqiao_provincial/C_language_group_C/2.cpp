30447fcc4f399fac1abdb040b1d8d20c8fae3261
comwrg <xcomwrg@gmail.com>
2018-03-21 08:25:59 +0800

    fix int overflow

--- past_paper_questions/2014_lanqiao_provincial/C_language_group_C/2.cpp
+++ past_paper_questions/2014_lanqiao_provincial/C_language_group_C/2.cpp
@@ -4,7 +4,8 @@ ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.goo
 void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
 
 bool valid(int n) {
-    int n3 = pow(n, 3);
+    long long n3 = pow(n, 3);
+    long long nn = n, nn3 = n3;
     while (n) {
         int i = n3 % 10; n3 /= 10;
         int j = n % 10;  n /= 10;
@@ -12,6 +13,7 @@ bool valid(int n) {
             return false;
         }
     }
+    cout << nn << " " << nn3 << endl;
     return true;
 }
 
@@ -20,7 +22,6 @@ int main() {
     for (int i = 1; i <= 10000; i++) {
         if (valid(i)) {
             n++;
-            cout << i << " ";
         }
     }
     cout << endl;
