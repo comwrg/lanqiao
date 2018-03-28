261a688453a0e2affb64be55331a3229e51dc411
comwrg <xcomwrg@gmail.com>
2018-03-28 15:39:10 +0800

    fix, holy shit, i realized i don't know what is leap year actually

--- past_paper_questions/蓝桥杯模拟二/3.cpp
+++ past_paper_questions/蓝桥杯模拟二/3.cpp
@@ -4,32 +4,34 @@ ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.goo
 void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
 
 int leap(int y) {
-    return ((y % 100 == 0 and y % 4 == 0) or (y % 100 != 0 and y % 400 == 0));
+    return ((y % 100 != 0 and y % 4 == 0) or (y % 400 == 0));
 }
 
-const int months[] = {0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int months[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main() {
     rdIn("data.txt");
 
     int y, m, d;
+    int sum = 0;
     while (~scanf("%d-%d-%d", &y, &m, &d)) {
         // 2017-02-23
         int ld = -(31 + 23) + d;
         for (int i = 2017; i < y; i++) {
-            ld += 366;
+            ld += 365;
             if (leap(i)) {
-                ld--;
+                ld++;
             }
         }
         for (int i = 1; i < m; i++) {
             ld += months[i];
             if (i == 2 and leap(y)) {
-                ld--;
+                ld++;
             }
         }
-        cout << ld << endl;
+        sum += ld;
     }
+    cout << sum;
 
   
     return 0;
