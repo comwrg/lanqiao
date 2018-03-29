17306ecf5d28539ac747490d59770fa596466925
comwrg <xcomwrg@gmail.com>
2018-03-29 18:17:11 +0800

    fix b

--- past_paper_questions/2018蓝桥杯省赛B组模拟赛一/b.cpp
+++ past_paper_questions/2018蓝桥杯省赛B组模拟赛一/b.cpp
@@ -5,7 +5,15 @@ int main() {
     bool stat[1001]; // not use 0
     fill(stat, stat+1001, 1);
     for (int i = 1; i < 1001; i++) {
-        if (i % 3 == 0 or i % 5 == 0 or i % 7 == 0) {
+        if (i % 3 == 0) {
+            stat[i] = !stat[i];
+        }
+
+        if (i % 5 == 0) {
+            stat[i] = !stat[i];
+        }
+
+        if (i % 7 == 0) {
             stat[i] = !stat[i];
         }
     }
