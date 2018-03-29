c04177f72352287cd5be126b78241b073312dc0a
comwrg <xcomwrg@gmail.com>
2018-03-29 10:11:32 +0800

    add question 7 code

--- past_paper_questions/蓝桥杯模拟二/7.cpp
+++ past_paper_questions/蓝桥杯模拟二/7.cpp
@@ -5,7 +5,31 @@ void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; 
 
 int main() {
     rdIn("data.txt");
-  
-  
+
+    string data;
+    cin >> data;
+    set<string> ans;
+
+    auto size = data.size();
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < 2; j++) {
+            int left_border, right_border;
+            // length of palindromic number is odd  if j == 0
+            // length of palindromic number is even if j == 1
+            left_border = right_border = i+j;
+            while (left_border >= 0 and right_border < size
+                   and data.at(left_border) == data.at(right_border)) {
+                ans.insert(data.substr(left_border, right_border-left_border+1));
+                left_border--; right_border++;
+            }
+        }
+    }
+
+    for (const auto &item : ans) {
+        cout << item << endl;
+    }
+
+
     return 0;
 }
