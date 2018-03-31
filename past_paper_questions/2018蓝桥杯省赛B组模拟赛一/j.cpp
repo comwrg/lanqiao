c08ef64fe88b5b8039bc6fee2893a8bf04c242c9
comwrg <xcomwrg@gmail.com>
2018-03-31 20:25:48 +0800

    add j TODO

---
+++ past_paper_questions/2018蓝桥杯省赛B组模拟赛一/j.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.good()) { cin.rdbuf(fin.rdbuf()); freopen(filename.c_str(), "r", stdin); } }
+void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
+
+int main() {
+    rdIn("data.txt");
+    // TODO
+
+    return 0;
+}
