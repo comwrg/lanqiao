03ab5e0264df1409dbb660897e438420c6c0940e
comwrg <xcomwrg@gmail.com>
2018-04-22 21:51:53 +0800

    hdu 1011, TLE

--- hdu/1011.cpp
+++ hdu/1011.cpp
@@ -3,18 +3,30 @@ using namespace std;
 ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.good()) { cin.rdbuf(fin.rdbuf()); freopen(filename.c_str(), "r", stdin); } }
 void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
 
-int data[101][2];
-vector<int> tunnel[101];
+const int NMAX = 101;
+int data[NMAX][2];
+vector<int> tunnel[NMAX];
+int dp[NMAX][NMAX];
 
-int dfs(int ntrooper, int room = 1) {
-    vector<int> ans {0};
+int dfs(int room, int ntrooper, int next = 0) {
     int rest = ntrooper - data[room][0];
-    if (rest < 0)
-        return -1;
-    for (int i : tunnel[room]) {
-        ans.push_back(dfs(rest, i));
+    if (rest < 0 or (data[room][0] == 0 and rest < 1))
+        return 0;
+
+    if (dp[room][ntrooper])
+        return dp[room][ntrooper];
+
+    int r = 0;
+    for (int i = next; i < tunnel[room].size(); ++i) {
+        int dst = tunnel[room].at(i);
+        for (int send = 0; send <= rest; ++send) {
+            r = max(r, dfs(room, rest-send, i+1) + dfs(dst, send));
+        }
     }
-    return data[room][1] + *max_element(ans.begin(), ans.end());
+    r += data[room][1];
+    if (next == 0)
+        dp[room][ntrooper] = r;
+    return r;
 }
 
 
@@ -24,7 +36,9 @@ int main() {
     int n, m;
     while (~scanf("%d %d", &n, &m) and ~n) {
         memset(data, 0, sizeof(data));
-        memset(tunnel, 0 , sizeof(tunnel));
+        memset(dp, 0, sizeof(dp));
+        for (auto &i : tunnel)
+            i.clear();
 
         for (int i = 1; i <= n; ++i) {
             scanf("%d %d", &data[i][0], &data[i][1]);
@@ -35,7 +49,7 @@ int main() {
             scanf("%d %d", &room, &dst);
             tunnel[room].push_back(dst);
         }
-        printf("%d\n", dfs(m));
+        printf("%d\n", dfs(1, m));
     }
 
     return 0;
