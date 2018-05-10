de0e796984c35aa331f180d50720dbcbe05a09ce
comwrg <xcomwrg@gmail.com>
2018-05-10 14:55:09 +0800

    D, WA

--- past_paper_questions/牛客练习赛16/D.cpp
+++ past_paper_questions/牛客练习赛16/D.cpp
@@ -3,40 +3,30 @@ using namespace std;
 ifstream fin; void rdIn(const string& filename) {fin.open(filename); if (fin.good()) { cin.rdbuf(fin.rdbuf()); freopen(filename.c_str(), "r", stdin); } }
 void debug(const char * __format, ...) { if (!fin.good()) return; va_list argv; __builtin_va_start(argv, __format); vprintf(__format, argv); va_end(argv); }
 
-const int NMAX = 1e5;
+const int NMAX = 1e5 + 10;
 // k base, b lucky number
 int k, b, n;
 int data[NMAX];
-int res[2*NMAX];
+int sum[NMAX];
 
 int main() {
     rdIn("data.txt");
 
     while (~scanf("%d %d %d", &k, &b, &n)) {
-        for (int i = 0; i < n; ++i) {
+        sum[0] = 0;
+        for (int i = 1; i <= n; ++i) {
             scanf("%d", data+i);
+            data[i] = 1 + (data[i]-1) % (k-1);
+            sum[i] = sum[i-1] + data[i];
         }
-
-
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                int idx = j-i+1;
-                memcpy(res, data+i, sizeof(int) * idx);
-                reverse(res, res+idx);
-                while (idx > 1) {
-                    for (int i = 1; i < idx; ++i) {
-                        res[0] += res[i];
-                    }
-                    idx = 0;
-                    while (res[idx]) {
-                        res[idx+1] = res[idx] / k;
-                        res[idx] %= k;
-                        ++idx;
-                    }
-                }
-                if (res[0] == b)
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int s = sum[i] - sum[j];
+                int dr = 1 + (s-1) % (k-1);
+                if (dr == b) {
                     ++ans;
+                }
             }
         }
         printf("%d\n", ans);
