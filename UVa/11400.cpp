f37c5155125e9fa371831bd1a78e426e630d32d8
comwrg <xcomwrg@gmail.com>
2018-05-18 10:19:54 +0800

    AC, order problem

--- UVa/11400.cpp
+++ UVa/11400.cpp
@@ -11,22 +11,24 @@ struct node {
     bool operator<(const node &rhs) {
         return v < rhs.v;
     }
-} data[1000];
+} data[1010];
 
 
 int main() {
-    int t, dp[1000], s[1000];
+    int t, dp[1010], s[1010];
     while (~scanf("%d", &t) and t) {
         s[0] = dp[0] = 0;
         for (int i = 1; i <= t; ++i) {
             scanf("%d %d %d %d", &data[i].v, &data[i].k, &data[i].c, &data[i].l);
+        }
+        sort(data+1, data+t+1);
+        for (int i = 1; i <= t; ++i) {
             s[i] = s[i-1] + data[i].l;
             dp[i] = dp[i-1] + data[i].k + data[i].c*data[i].l;
         }
-        sort(data, data+t);
         for (int i = 1; i <= t; ++i) {
             for (int j = 0; j < i; ++j) {
-                dp[i] = min(dp[i], dp[j] + (s[i]-s[j])*data[i].c + data[i].k);
+                dp[i] = min(dp[i], dp[j] + data[i].k + (s[i]-s[j])*data[i].c);
             }
         }
         printf("%d\n", dp[t]);
